50964ae0f06c027929764347b095d7e04f49ffc3
souhe <souhepend@hotmail.com>
2014-12-18 11:47:00 +0100

    Defects fixed

--- src/js/mailArea.js
+++ src/js/mailArea.js
@@ -20,7 +20,7 @@ var MailArea = function (template, emailList, maxInputWidth, styleInactive, styl
     this.AdditionnalStyleUnApproved = styleUnApproved;
     this.stylesAll = this.styleInactive + " " + this.styleApproved + " " + this.styleEdit + " " + this.styleUnApproved;
 
-    if(emails && emails.length > 0){
+    if (emails && emails.length > 0) {
         for (var i = 0; i < emails.length; i++){
             this.addEmailField(emails[i]);
         }
@@ -118,34 +118,32 @@ MailArea.prototype.removeEmail = function (field) {
 MailArea.prototype.addEmail = function (email, isInitial) {
     var curr = this;
     email.val($.trim(email.val()));
+    var value = email.val();
+    var lastProperVal = email.data("lastProperVal");
+    
     if (email) {
-        if (email.val() === "") { //if email is empty
+        if (value === "") { //if email is empty
             setTimeout(function() {
                 curr.removeEmail(email.parents('.email-container'));
             }, 200);
             
-            if(typeof this.events.onRemove === "function"){
-                this.events.onRemove(email.val());
+            if(lastProperVal && typeof this.events.onRemove === "function"){
+                this.events.onRemove(value);
             }
         } else {  
             if (this.validate(email)) {
-                var value = email.val();
-                var lastProperVal = email.data("lastProperVal");
-                
                 email.parents('.email-container').removeClass(this.stylesAll).addClass(this.styleApproved);
                 if(!lastProperVal){
                     if(!isInitial && typeof this.events.onAdd === "function"){
                         this.events.onAdd(value);
                     }
                 }else if(value !== lastProperVal) {
-                    
-                    
                     if(!isInitial && typeof this.events.onAdd === "function"){
                         this.events.onChange(lastProperVal, value);
                     }
                 }
                 
-                email.data("lastProperVal", value)
+                email.data("lastProperVal", value);
             } else {
                 email.parents('.email-container').removeClass(this.stylesAll).addClass(this.styleUnApproved);
             }
@@ -175,14 +173,17 @@ MailArea.prototype.getEmails = function () {
 MailArea.prototype.getEmailsIfValid = function(){
     var emailList = [];
     var curr = this;
+    var isValid = true;
     this.$emailList.children('.email-container').each(function () {
         var input = $(this).find('input');
         if (!curr.validate(input)) {
             emailList.push(input.val().toString());
-            return false;
+            if(input.val()){
+                isValid = false;
+            }
         }
     });
-    return emailList;
+    return isValid ? emailList : false;
 };
 
 MailArea.prototype.validateEmail = function (input) {
@@ -190,25 +191,6 @@ MailArea.prototype.validateEmail = function (input) {
     var regex = /^((((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))|([^<>@]+\<(((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))\>))$/i;
     return regex.test(email);
 };
-
-function resolveEvent(event, args){
-    var deferred = $.Deferred();
-    
-    if(typeof event === "function"){
-        var args = [].splice.apply(arguments, [1,  arguments.length -1]);
-        var promise = event.apply(this, args);
-        if(typeof promise.then === 'function'){
-            deferred.notify();
-            promise.then(function(){
-                deferred.resolve();
-            }, function(){
-                deferred.reject();
-            })
-        }
-    }
-    
-    return deferred.promise();
-}
 
 var methods = {
     settings: {
